bc786408156f1e8b275de589890679647c96a822
Koperczak <akopercz@purdue.edu>
2023-10-12 12:06:36 -0400

    fixed exporting script

--- Schematic/recolor_schematic.py
+++ Schematic/recolor_schematic.py
@@ -1,6 +1,7 @@
 import re
 
-FILES = ("Schematic-F_Cu.svg", "Schematic-B_Cu.svg")
+FILES = ((["Schematic-F_Cu.svg", "Schematic-F_Fab.svg", "Schematic-F_Silkscreen.svg", "Schematic-Edge_Cuts.svg"], "Schematic-Front.svg"),
+         (["Schematic-B_Cu.svg", "Schematic-B_Fab.svg", "Schematic-B_Silkscreen.svg", "Schematic-Edge_Cuts.svg"], "Schematic-Back.svg"))
 
 F_CU  = re.compile("C83434"), "A00000"
 F_FAB = re.compile("AFAFAF"), "00A0FF"
@@ -8,6 +9,7 @@ B_CU  = re.compile("4D7FC4"), "4D7FC4"
 B_FAB = re.compile("585D84"), "FFFF00"
 BACKGROUND = r"""<rect x="0" y="0" width="100%" height="100%" style="fill:#222222"/>"""
 BACKGROUND_LINE = 12 - 1
+SKIP_LINES      = 12 - 1
 
 def do_replace(data, reg):
     return reg[0].sub(reg[1], data)
@@ -17,9 +19,20 @@ def add_backgroud(data):
     data = data[:BACKGROUND_LINE] + [BACKGROUND] + data[BACKGROUND_LINE:]
     return "\n".join(data)
 
-def run_on_file(filename):
-    with open(filename, "r") as file:
+def combine_files(main, new):
+    with open(new) as file:
+        data = file.readlines()
+    
+    main = main.splitlines()
+    main = main[:-2] + data[SKIP_LINES:-1] + main[-2:]
+    return "\n".join(main)
+
+def run_on_files(filenames, output):
+    with open(filenames[0], "r") as file:
         data = file.read()
+        
+    for file in filenames[1:]:
+        data = combine_files(data, file)
 
     data = do_replace(data, F_CU)
     data = do_replace(data, F_FAB)
@@ -27,9 +40,9 @@ def run_on_file(filename):
     data = do_replace(data, B_FAB)
     data = add_backgroud(data)
 
-    with open(filename, "w") as file:
+    with open(output, "w") as file:
         file.write(data)
         
 if __name__ == "__main__":
-    for filename in FILES:
-        run_on_file(filename)
+    for filenames, output in FILES:
+        run_on_files(filenames, output)
